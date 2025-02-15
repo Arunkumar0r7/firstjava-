23fbb94cc6063af813b9fc676f4e3477a7928583
karthikeyan <arunremy71@gmail.com>
2025-02-15 10:31:01 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,165 @@
+//normally printing hello world
+
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("hello world ");
+    }
+}
+
+ data types
+//primitivee data type
+
+byte
+int
+short
+long
+double
+float
+boolean
+char
+
+public class Main{
+    public static void main(String[] args) {
+        byte a = 127;
+        System.out.println(a);
+    }
+}
+
+public class  Main{
+    public static void main(String[] args) {
+        short a = 32000;
+        System.out.println(a);
+
+    }
+}
+
+public class Main{
+    public static void main(String[] args) {
+        long a = 20000000000l;
+        System.out.println(a);
+    }
+}
+
+public class Main{
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+        int c = 50;
+        int d = (a+b+c);
+        System.out.println(d);
+    }
+}
+
+// to store decimal values
+
+public class Main {
+    public static void main(String[] args) {
+        float d = 92.5f;
+        System.out.println( d);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        double d = 95.555555;
+        System.out.println(d);
+    }
+}
+
+public class Main{
+    public static void main(String[] args) {
+        char a = 'b';
+        System.out.println(a);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        boolean arun = false;
+        if(arun)
+            System.out.println("yes");
+        else{
+            System.out.println("bad");
+        }
+    }
+}
+
+// reference data type
+//string
+//array
+// class and object
+
+/*public class Main {
+    public static void main(String[] args) {
+        String name ="Arun";
+        System.out.println(name);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers= { 1,2,3,4,5};
+        for (int i=0;i< numbers.length;i++){
+            System.out.println(i);
+        }
+
+    }
+}
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+// control statments
+public class Main {
+    public static void main(String[] args) {
+        Stri
+
+        ng a = "dinesh";
+        String b = "dinesh";
+        if(a==b) {
+            System.out.println("arun is tester");
+        }
+            else{
+                System.out.println("dinesh is develepor");
+            }
+    }
+
+    }
+
+public class Main
+{
+    public static void main(String[] args) {
+        int arun = 100;
+        int karthi = 30;
+
+        if(arun<karthi){
+            System.out.println("Arun is gethu");
+        }
+        else if(30<=29){
+            System.out.println("karthi is gethu");
+        }
+        else{
+            System.out.println("renduperumea waste");
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 1;
+        switch (a) {
+            case 1:
+                System.out.println("this is one ");
+                break;
+            case 2:
+                System.out.println(" this is two");
+        }
+    }
+}*/
+
+
+
+
+
+
+
+
---
+++ src/Main2.java
@@ -0,0 +1,31 @@
+/*for loop
+public class Main2 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Arun");
+        }
+    }
+}
+
+//while loop
+public class Main2{
+    public static void main(String[] args) {
+        int i =1;
+        while(i<= 5){
+            i++;
+            System.out.println("arun");
+        }
+    }
+}
+
+//do while loop
+public class Main2{
+    public static void main(String[] args) {
+        int i=1;
+        do {
+            System.out.println("arun");
+            i++;
+        }while(i<=10);
+        }
+    }*/
